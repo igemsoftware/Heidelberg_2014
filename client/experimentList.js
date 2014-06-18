77c2bd5f3f942ad8a615f2d7fd46eea036184eba
Silvan Schmitz <silvan@silvanschmitz.de>
2014-06-19 01:04:44 +0200

    bootstrapped experiments list

--- client/experimentList.js
+++ client/experimentList.js
@@ -3,3 +3,19 @@
 UI.registerHelper('experiments', function () {
 	return Experiments.find({ }, { sort: { finishDate: -1 } });
 });
+
+Template.experimentList.products = function () {
+	return _.values(this.products);
+}
+
+Template.experimentList.protocol = function () {
+	return Protocols.findOne(this.protocol._id);
+}
+
+Template.experimentList.productText = function (experiment) {
+	var texts = [];
+	_.each(experiment.products[CryptoJS.MD5(this.name).toString()], function (productType) {
+		if (productType.text) texts.push(productType.text);
+	});
+	return texts.join('/');
+}
