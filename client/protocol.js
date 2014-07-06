igemsoftware/Heidelberg_2014

commit 3acf9302fec70eeb0e26c53dec655e33a4562b36

#4 first round of object model cleanup (everything except experiments)

client/protocol.js
@@ -1,11 +1,218 @@
 'use strict';
 
+function ProtocolVM(data) {
+	var self = this;
+	self.editMode = data.editMode;
+	Protocol.call(this, data.protocol(), ko.unwrap(data.version), self.editMode());
+
+	self.versions = self.DBData && _.map(self.DBData.v, function (version, index) {
+		return new VersionVM(version, index, Router.path('viewProtocol', { id: self._id() }, { query: { v: index } }));
+	}).reverse();
+
+	self.multiParams = ko.computed(function () {
+		return _.filter(self.params(), function (param) {
+			return param.multi();
+		});
+	});
+
+	self.performHref = Router.path('performProtocol', { id: self._id() });
+}
+ProtocolVM.prototype = new Protocol();
+ProtocolVM.prototype.constructor = ProtocolVM;
+
+ProtocolVM.prototype.edit = function () {
+	Router.go('viewProtocol', { id: this._id() }, { query: { edit: 1 } });
+};
+
+ProtocolVM.prototype.save = function () {
+	var self = this;
+	var flat;
+	try {
+		flat = self.flatten();
+	} catch (e) {
+		// The result being thrown as an exception signals a failed cascading update, but this doesn't matter here
+		// Check if the exception really is the result first
+		if (!e.name instanceof Function || e.name() != protocol.name) throw e;
+		flat = e;
+	}
+
+	if (!self._id()) {
+		Meteor.call('insertProtocol', flat, function (error, result) {
+			if (!error) {
+				self._id(result);
+				Router.go('viewProtocol', { id: self._id() });
+			}
+		});
+	} else if (!isUnchanged(flat, self.DBData)) {
+		var xs = Experiments.find({ 'protocol._id': self._id(), 'protocol.v': self.DBData.v.length - 1 });
+		var xCount = xs.count();
+		if (xCount > 0) {
+			var cannotUpdate = false;
+			var needsMapping = false;
+
+			var paramMappings = [];
+			if (!isUnchanged(flat.params, self.DBData.params)) {
+				var oldParams = { };
+				var oldParamsByType = { };
+				_.each(self.DBData.params, function (oldParam) {
+					oldParam = _.clone(oldParam);
+					oldParam.allTypes = _.pluck(Types.findOne(oldParam.type).allBaseTypes, '_id');
+					oldParam.allTypes.push(oldParam.type._id);
+					oldParam.allTypesMap = { };
+
+					_.each(oldParam.allTypes, function (typeId) {
+						oldParam.allTypesMap[typeId] = 1;
+						if (!oldParamsByType[typeId]) oldParamsByType[typeId] = { 0: [], 1: [] };
+						oldParamsByType[typeId][oldParam.multi ? 1 : 0].push(oldParam);
+					});
+					oldParams[oldParam.name] = oldParam;
+				});
+
+				_.each(self.params(), function (param) {
+					var oldParam = oldParams[param.name()];
+					var typeMatchingOldParams = _.clone(oldParamsByType[param.type()._id()]) || { 0: [], 1: [] };
+					_.each(param.type().allBaseTypes(), function (type) {
+						if (oldParamsByType[type._id()]) {
+							Array.prototype.push.apply(typeMatchingOldParams[0], oldParamsByType[type._id()][0]);
+							Array.prototype.push.apply(typeMatchingOldParams[1], oldParamsByType[type._id()][1]);
+						}
+					});
+
+					if (!typeMatchingOldParams[0] && !param.multi()) {
+						cannotUpdate = true;
+					} else {
+						var obj = {
+							param: param,
+							possibleMappings: typeMatchingOldParams[param.multi() ? 1 : 0] || []
+						};
+						obj.source = ko.observable(_.contains(obj.possibleMappings, oldParam) && oldParam);
+						obj.blank = ko.observable(param.multi() && !obj.source.peek());
+						paramMappings.push(obj);
+						if (!param.multi() && (!oldParam || !oldParam.allTypesMap[param.type()._id()] || !oldParam.allTypesMap[param.type()._id()][0])) needsMapping = true;
+					}
+				});
+			}
+
+			var inputMappings = [];
+			if (!isUnchanged(flat.steps, self.DBData.steps)) {
+				var oldInputs = { };
+				var oldInputsByDesc = { };
+				var oldInputsByType = { };
+
+				_.each(self.DBData.steps, function (oldStep, oldStepIdx) {
+					oldInputs[oldStep.desc] = { };
+					_.each(oldStep.inputs, function (oldInput, oldInputIdx) {
+						oldInput = _.clone(oldInput);
+						oldInput.stepIdx = oldStepIdx;
+						oldInput.inputIdx = oldInputIdx;
+						oldInput.longDesc = oldInput.desc + ' of step "' + oldStep.desc + '"';
+						oldInputs[oldStep.desc][oldInput.desc] = oldInput;
+						if (!oldInputsByDesc[oldInput.desc]) oldInputsByDesc[oldInput.desc] = { };
+						oldInputsByDesc[oldInput.desc][oldStepIdx] = oldInput;
+						if (!oldInputsByType[oldInput.type]) oldInputsByType[oldInput.type] = [];
+						oldInputsByType[oldInput.type].push(oldInput);
+					});
+				});
+
+				_.each(self.steps(), function (step, stepIdx) {
+					_.each(step.inputs(), function (input, inputIdx) {
+						var oldInput = oldInputs[step.desc()] && oldInputs[step.desc()][input.desc()];
+						if (input.required() && !oldInputsByType[input.type().id]) {
+							cannotUpdate = true;
+						} else {
+							var obj = {
+								stepIdx: stepIdx,
+								step: step,
+								inputIdx: inputIdx,
+								input: input,
+								possibleMappings: oldInputsByType[input.type().id],
+								source: ko.observable(oldInput || (oldInputsByDesc[input.desc()] && oldInputsByDesc[input.desc()][stepIdx])),
+							};
+							obj.blank = ko.observable(!input.required() && !obj.source.peek());
+							inputMappings.push(obj);
+							if (!oldInput || oldInput.type != input.type().id) needsMapping = true;
+						}
+					});
+				});
+			}
+
+			if (cannotUpdate) {
+				self.cannotCascadeModalVM({
+					protocol: self,
+					xCount: xCount,
+					save: function () {
+						Meteor.call('updateProtocol', self._id(), flat, self.DBData.v.length - 1);
+						$('#cannotCascadeModal').on('hidden.bs.modal', function () {
+							Router.go('viewProtocol', { id: self._id() });
+						});
+					}
+				});
+				$('#cannotCascadeModal').modal();
+			} else if (needsMapping) {
+				self.mappingsModalVM({
+					protocol: self,
+					xCount: xCount,
+					cascade: ko.observable(true),
+					paramMappings: paramMappings,
+					inputMappings: inputMappings,
+					save: function () {
+						if (this.cascade()) {
+							var flatParamMappings = [];
+							_.each(paramMappings, function (paramMapping) {
+								if (!paramMapping.blank()) flatParamMappings.push({ param: paramMapping.param.name(), source: paramMapping.source().name });
+							});
+
+							var inputMappingsMap = { };
+							_.each(inputMappings, function (inputMapping) {
+								if (!inputMapping.blank()) {
+									if (!inputMappingsMap[inputMapping.stepIdx]) inputMappingsMap[inputMapping.stepIdx] = { };
+									inputMappingsMap[inputMapping.stepIdx][inputMapping.inputIdx] = { step: inputMapping.source().stepIdx, input: inputMapping.source().inputIdx };
+								}
+							});
+
+							flat.versionMappings = {
+								params: flatParamMappings,
+								inputs: inputMappingsMap,
+							};
+							Meteor.call('updateProtocol', self._id(), flat, self.DBData.v.length - 1);
+						} else {
+							Meteor.call('updateProtocol', self._id(), flat, self.DBData.v.length - 1);
+						}
+						$('#mappingsModal').on('hidden.bs.modal', function () {
+							Router.go('viewProtocol', { id: self._id() });
+						});
+					}
+				});
+				$('#mappingsModal').modal();
+			} else {
+				Meteor.call('updateProtocol', self._id(), flat, self.DBData.v.length - 1);
+				Router.go('viewProtocol', { id: self._id() });
+			}
+		} else {
+			Meteor.call('updateProtocol', self._id(), flat, self.DBData.v.length - 1);
+			Router.go('viewProtocol', { id: self._id() });
+		}
+	}
+};
+
+ProtocolVM.prototype.cancel = function () {
+	if (this._id()) {
+		Router.go('viewProtocol', { id: this._id() });
+	} else {
+		Router.go('protocolList');
+	}
+};
+
+ProtocolVM.prototype.displayVersions = function() {
+	$("#versionModal").modal();
+};
+
 Template.protocol.rendered = function () {
 	var self = this;
 	var cannotCascadeModalVM = ko.observable();
 	var mappingsModalVM = ko.observable();
 	self.vm = ko.computed(function () {
-		var vm = new protocolVM(self.data);
+		var vm = new ProtocolVM(self.data);
 		vm.cannotCascadeModalVM = cannotCascadeModalVM;
 		vm.mappingsModalVM = mappingsModalVM;
 		return vm;

client/supplies.js
@@ -8,10 +8,68 @@ Template.suppliesList.text = function () {
 	return _.pluck(this.types, 'text').join('/');
 };
 
+function SupplyVM(data) {
+	var self = this;
+	self.editMode = data.editMode;
+	Supply.call(this, data.supply(), ko.unwrap(data.version), self.editMode());
+
+	self.versions = self.DBData ? _.map(self.DBData.v, function (version, index) {
+		return new VersionVM(version, index, Router.path('viewSupply', { id: self._id() }, { query: { v: index } }));
+	}).reverse() : [];
+
+	self.dateUpdate = !self.DBData && setInterval(function () {
+		self.date(new Date());
+	}, 1000);
+}
+SupplyVM.prototype = new Supply();
+SupplyVM.prototype.constructor = SupplyVM;
+
+SupplyVM.prototype.edit = function () {
+	Router.go('viewSupply', { id: this._id() }, { query: { edit: 1 } });
+};
+
+SupplyVM.prototype.save = function () {
+	var self = this;
+	clearInterval(self.dateUpdate);
+	var flat;
+	try {
+		flat = self.flatten();
+	} catch (e) {
+		// The result being thrown as an exception signals a failed cascading update, but this doesn't matter here
+		// Check if the exception really is the result first
+		if (!e.name instanceof Function || e.name() != protocol.name) throw e;
+		flat = e;
+	}
+
+	if (!this._id()) {
+		Meteor.call('insertSupply', flat, function (error, result) {
+			if (!error) {
+				self._id(result);
+				Router.go('viewSupply', { id: self._id() });
+			}
+		});
+	} else if (!isUnchanged(flat, this.DBData)) {
+		Meteor.call('updateSupply', this._id(), flat);
+		Router.go('viewSupply', { id: this._id() });
+	}
+};
+
+SupplyVM.prototype.cancel = function () {
+	if (this._id()) {
+		Router.go('viewSupply', { id: this._id() });
+	} else {
+		Router.go('suppliesList');
+	}
+};
+
+SupplyVM.prototype.displayVersions = function() {
+	$("#versionModal").modal();
+};
+
 Template.supply.rendered = function () {
 	var self = this;
 	self.vm = ko.computed(function () {
-		return new supplyVM(self.data);
+		return new SupplyVM(self.data);
 	});
 
 	self.nodesToClean = [];

lib/supply.js
@@ -1,0 +1,155 @@
+Supply = function (data, version, tryUpdate) {
+	var pTypeMap = { };
+	this.possibleTypes = _.map(Types.find().fetch(), function (type) {
+		return pTypeMap[type._id] = new Type(type);
+	}); // TODO
+
+	var self = this;
+	self._id = ko.observable(data && data._id);
+	self.version = version === undefined ? data && data.v.length - 1 : version;
+	self.oldVersion = data && self.version != data.v.length - 1;
+	self.DBData = data;
+	self.data = data && data.v[self.version];
+
+	self.needsReview = ko.observable(data && data.needsReview);
+
+	var typeMappings = { };
+	self.types = ko.observableArray(self.DBData ? _.map(self.DBData.types, function (type) {
+		var obj = pTypeMap[type._id];
+		if (!tryUpdate && type.v != obj.version) obj = new Type(Types.findOne(type._id), type.v);
+		if (tryUpdate && type.v != obj.version && obj.DBData.versionMappings) {
+			_.each(obj.DBData.versionMappings, function (mapping) {
+				if (!typeMappings[mapping.property.from._id]) typeMappings[mapping.property.from._id] = { };
+				typeMappings[mapping.property.from._id][mapping.property.name] = mapping.source;
+			});
+		}
+		return obj;
+	}) : []);
+
+	var propertyMap = { };
+	self.autoUpdateFailed = false; // only meaningful when tryUpdate == true
+	self.properties = ko.computed(function () {
+		var map = { };
+		_.each(self.types(), function (type) {
+			_.each(type.allProperties(), function (property) {
+				var fromId = property.from._id();
+				var name = property.name();
+				var md5Name = CryptoJS.MD5(name).toString();
+
+				if (!propertyMap[fromId]) propertyMap[fromId] = { };
+
+				var source;
+				if (typeMappings[fromId] && typeMappings[fromId][md5Name]) {
+					source = self.DBData.properties[typeMappings[fromId][md5Name].from._id]
+						&& self.DBData.properties[typeMappings[fromId][md5Name].from._id][CryptoJS.MD5(typeMappings[fromId][md5Name].name).toString()];
+					if (!source && property.required()) {
+						self.autoUpdateFailed = false;
+					}
+				} else if (self.DBData && self.DBData.properties[fromId]) {
+					source = self.DBData.properties[fromId][md5Name];
+				}
+
+				if (!propertyMap[fromId][name]) propertyMap[fromId][name] = new SupplyProperty(this, property, source);
+
+				if (!map[fromId]) map[fromId] = { };
+				map[fromId][name] = propertyMap[fromId][name];
+			});
+		});
+
+		return _.flatten(_.map(map, _.values));
+	});
+
+	self.source = ko.observable(self.DBData && self.DBData.source);
+	self.date = ko.observable(self.DBData ? self.DBData.date : new Date());
+	self.notes = ko.observable(self.DBData && self.DBData.notes);
+};
+
+Supply.prototype.toReference = function () {
+	return self._id() && {
+		_id: self._id(),
+		v: self.version,
+	};
+};
+
+Supply.prototype.flatten = function () {
+	var self = this;
+
+	var allTypes = { };
+	_.each(this.types(), function (type) {
+		allTypes[type._id()] = type.toReference();
+		_.each(type.allBaseTypes(), function (baseType) {
+			allTypes[baseType._id()] = baseType.toReference();
+		});
+	});
+
+	var values = { };
+	_.each(self.properties(), function (property) {
+		if (!values[property.property.from._id()]) values[property.property.from._id()] = { };
+		values[property.property.from._id()][property.property.name()] = property.value;
+	});
+
+	var flat = {
+		types: _.map(self.types(), function (type) {
+			return _.extend({
+				text: type.getText(values),
+			}, type.toReference());
+		}),
+		allTypes: _.values(allTypes),
+		properties: _.map(self.properties(), function (property) {
+			return property.flatten();
+		}),
+		source: self.source(),
+		date: self.date(),
+		notes: self.notes(),
+	};
+
+	if (self.autoUpdateFailed) throw flat; // TODO: Not very elegant ...
+	return flat;
+};
+
+SupplyProperty = function (supply, property, data) {
+	this.parent = supply;
+
+	this.property = property;
+	this.value = ko.observable(data && data.value);
+};
+
+SupplyProperty.prototype.flatten = function () {
+	return {
+		from: this.property.from.toReference(),
+		name: this.property.name(),
+		value: this.value()
+	};
+};
+
+Meteor.methods({
+	insertSupply: function (flat) {
+		if (!this.userId) throw 403;
+
+		flat.editor = this.userId;
+		flat.mtime = new Date();
+		return Supplies.insert(_.extend({
+			v: [flat],
+		}, flat));
+	},
+	updateSupply: function (id, newVersion) {
+		if (!this.userId) throw 403;
+
+		newVersion.editor = this.userId;
+		newVersion.mtime = new Date();
+
+		Supplies.update(id, { $push: { v: newVersion }, $set: newVersion, $unset: { needsReview: true } }, function (error) {
+			Experiments.find({ sourceSupplies: id }).forEach(function (dependentExperiment) {
+				var protocol = Protocols.findOne(dependentExperiment.protocol._id);
+				var vm = new experimentVM({ experiment: dependentExperiment, protocol: protocol }, undefined, true);
+				vm.save(dependentExperiment.v[dependentExperiment.v.length - 1].performer, {
+					type: 'cascade',
+					dependency: {
+						type: 'supply',
+						id: id
+					}
+				});
+			});
+		});
+	}
+});

lib/type.js
@@ -1,0 +1,282 @@
+Type = function (data, version) {
+	var self = this;
+	self._id = ko.observable(data && data._id);
+	if (data) self.typeMap[data._id](this); // TODO
+	self.version = version === undefined ? data && data.v.length - 1 : version;
+	self.oldVersion = data && self.version != data.v.length - 1;
+	self.DBData = data;
+	self.data = data && data.v[self.version];
+
+	self.name = ko.observable(self.data && self.data.name);
+	self.baseTypes = ko.observableArray(self.data ? _.map(self.data.baseTypes, function (baseType) {
+		return self.typeMap[baseType._id].peek();
+	}) : []);
+
+	self.allBaseTypes = ko.computed(function () {
+		var allBaseTypes = { };
+
+		_.each(self.baseTypes(), function (baseType) {
+			allBaseTypes[baseType._id()] = baseType;
+			_.each(baseType.allBaseTypes(), function (baseBaseType) {
+				allBaseTypes[baseBaseType._id()] = baseBaseType;
+			});
+		});
+
+		return _.values(allBaseTypes);
+	});
+
+	self.properties = ko.observableArray(self.data ? _.map(self.data.properties, function (property) {
+		return new TypeProperty(self, property);
+	}) : []);
+	self.inheritedProperties = ko.computed(function () {
+		var arr = [];
+		var map = { };
+		_.each(self.baseTypes(), function (type) {
+			var usedTypes = { };
+			usedTypes[type._id()] = 1;
+
+			_.each(type.allProperties(), function (property) {
+				var typeid = (property.from || type)._id();
+				if (map[typeid] != 1) {
+					arr.push(property);
+					usedTypes[typeid] = 1;
+				}
+			});
+
+			_.extend(map, usedTypes);
+		});
+
+		return arr;
+	});
+	self.allProperties = ko.computed(function () {
+		var arr = _.clone(self.properties());
+		Array.prototype.push.apply(arr, self.inheritedProperties());
+		return arr;
+	});
+
+	self.text = ko.observable(self.data && self.data.text);
+};
+
+Type.prototype.href = function () {
+	return Router.path('viewType', { id: this._id() });
+};
+
+Type.prototype.getText = function (values) {
+	var self = this;
+	var text = '';
+	_.each(self.text(), function (part) {
+		switch (part.type) {
+			case 'text':
+				text = text + part.text;
+				break;
+			case 'propertyReference':
+				text = text + ko.unwrap(values[ko.unwrap((part.property.from || self)._id)][part.property.name]);
+				break;
+		}
+	});
+	return text;
+};
+
+Type.prototype.addProperty = function () {
+	this.properties.push(new TypeProperty(this));
+};
+
+Type.prototype.removeProperty = function (property) {
+	this.properties.remove(property);
+};
+
+Type.prototype.toReference = function () {
+	var id = this._id();
+	return id && {
+		_id: id,
+		name: this.name(),
+		v: this.version
+	};
+};
+
+Type.prototype.flatten = function () {
+	return {
+		name: this.name(),
+		baseTypes: _.map(this.baseTypes(), function (baseType) {
+			return baseType.toReference();
+		}),
+		allBaseTypes: _.map(this.allBaseTypes(), function (baseType) {
+			return baseType.toReference();
+		}),
+		properties: _.map(this.properties(), function (property) {
+			return property.flatten();
+		}),
+		allProperties: _.map(this.allProperties(), function (property) {
+			return property.flatten();
+		}),
+		text: this.text(),
+	};
+};
+
+Type.prototype.tryUpdate = function (baseMappings) {
+	if (this.oldVersion) throw "tryUpdate cannot be called on old versions.";
+	if (!baseMappings) return true;
+
+	_.each(flattened.text, function (part, idx) {
+		if (part.type == 'propertyReference' && part.property.from) {
+			var matching = _.filter(baseMappings, function (mapping) {
+				return (mapping.source.from || mapping.property.from)._id == part.property.from._id && mapping.source.name == part.property.name;
+			});
+			// TODO: implement failure of cascading type updates in a safe way
+			//if (matching.length == 1) {
+				part = _.clone(part);
+				part.property = matching[0].property;
+				flattened.text[idx] = part;
+			/*} else if (matching.length != 0) {
+				fail_save();
+			}*/
+		}
+	});
+	this.versionMappings = baseMappings;
+	return true;
+};
+
+TypeProperty = function (from, data) {
+	this.from = from;
+
+	this.name = ko.observable(data ? data.name : '');
+	this.type = ko.observable(data ? PODType.typeMap[data.type] : PODType.types[0]);
+	this.required = ko.observable(data ? data.required : true);
+};
+
+TypeProperty.prototype.types = PODType.types;
+
+TypeProperty.prototype.toReference = function () {
+	return {
+		name: this.name(),
+		from: this.from.toReference(),
+	};
+};
+
+TypeProperty.prototype.flatten = function () {
+	return {
+		name: this.name(),
+		type: this.type().id,
+		required: this.required(),
+		from: this.from.toReference(),
+		versionMappings: this.versionMappings,
+	};
+};
+
+function reloadTypes() {
+	// TODO
+	Type.prototype.typeMap = { };
+	var types = Types.find().fetch();
+	_.each(types, function (type) {
+		Type.prototype.typeMap[type._id] = ko.observable(ko.mapping.fromJS(type));
+	});
+	Type.prototype.types = _.map(types, function (type) {
+		return new Type(type);
+	});
+
+}
+if (Meteor.isClient) {
+	Deps.autorun(reloadTypes);
+} else {
+	reloadTypes();
+}
+
+Meteor.methods({
+	insertType: function (flat) {
+		if (!this.userId) throw 403;
+
+		flat.editor = this.userId;
+		flat.mtime = new Date();
+		return Types.insert(_.extend({
+			v: [flat],
+		}, flat));
+		reloadTypes(); // TODO
+	},
+	updateType: function (id, newVersion) {
+		if (!this.userId) throw 403;
+
+		newVersion.editor = this.userId;
+		newVersion.mtime = new Date();
+
+		var changeType = {
+			type: 'cascade',
+			dependency: {
+				type: 'type',
+				id: id,
+				changeType: newVersion.changeType,
+			}
+		};
+
+		Types.update(id, { $push: { v: newVersion }, $set: newVersion }, function (error) {
+			reloadTypes(); // TODO
+
+			Types.find({ 'allBaseTypes._id': id }).forEach(function (dependentType) {
+				var obj = new Type(dependentType);
+				obj.tryUpdate(newVersion.versionMappings);
+				var flat = obj.flatten();
+				if (!isUnchanged(flat, dependentType)) {
+					Meteor.call('updateType', dependentType._id, _.extend({ changeType: changeType }, flat));
+				}
+			});
+
+			Supplies.find({ 'types._id': id, needsReview: { $exists: false } }).forEach(function (supply) {
+				var obj = new Supply(supply, undefined, true);
+				var flat;
+				try {
+					flat = obj.flatten();
+				} catch (e) {
+					// An exception signals a failed cascading update; just set needsReview
+					Supplies.update(supply._id, { $set: { needsReview: true } }, { multi: true });
+				}
+				if (flat && !isUnchanged(flat, supply)) {
+					Meteor.call('updateSupply', supply._id, _.extend({ changeType: changeType }, flat));
+				}
+			});
+
+			Protocols.find({ $or: [{ 'params.type._id': id }, { 'products.types._id': id }], needsReview: { $exists: false } }).forEach(function (protocol) {
+				var obj = new Protocol(protocol, undefined, true);
+				var flat;
+				try {
+					flat = obj.flatten();
+				} catch (e) {
+					// The result being thrown as an exception signals a failed cascading update; just set needsReview
+					// Check if the exception really is the result first
+					if (!e.name instanceof Function || e.name() != protocol.name) throw e;
+					Protocols.update(protocol._id, { $set: { needsReview: true } }, { multi: true });
+				}
+				if (flat && !isUnchanged(flat, protocol)) {
+					Meteor.call('updateProtocol', protocol._id, _.extend({ changeType: changeType }, flat), protocol.v.length - 1);
+				}
+			});
+		});
+	},
+	updateTypeWithoutCascade: function (id, newVersion) {
+		if (!this.userId) throw 403;
+
+		newVersion.editor = this.userId;
+		newVersion.mtime = new Date();
+
+		var changeType = {
+			type: 'cascade',
+			dependency: {
+				type: 'type',
+				id: id,
+				changeType: newVersion.changeType,
+			}
+		};
+
+		Types.update(id, { $push: { v: newVersion }, $set: newVersion }, function (error) {
+			Types.find({ 'allBaseTypes._id': id }).forEach(function (dependentType) {
+				var obj = new Type(dependentType);
+				obj.tryUpdate(newVersion.versionMappings);
+				var flat = obj.flatten();
+				if (!isUnchanged(flat, dependentType)) {
+					Meteor.call('updateTypeWithoutCascade', dependentType._id, _.extend({ changeType: changeType }, flat));
+				}
+			});
+		});
+
+		Supplies.update({ 'types._id': id }, { $set: { needsReview: true } }, { multi: true });
+		Protocols.update({ $or: [{ 'params.type._id': id }, { 'products.types._id': id }] }, { $set: { needsReview: true } }, { multi: true });
+	}
+});

commit 1caa92bea43d11b7130619b6ab4096c52484d632

fixed wrong format in Supply.prototype.flatten introduced in 3acf930; finally right error check on cascade failure

client/protocol.js
@@ -32,7 +32,7 @@ ProtocolVM.prototype.save = function () {
 	} catch (e) {
 		// The result being thrown as an exception signals a failed cascading update, but this doesn't matter here
 		// Check if the exception really is the result first
-		if (e.name != self.name()) throw e;
+		if (e instanceof Error) throw e;
 		flat = e;
 	}
 

client/supplies.js
@@ -37,7 +37,7 @@ SupplyVM.prototype.save = function () {
 	} catch (e) {
 		// The result being thrown as an exception signals a failed cascading update, but this doesn't matter here
 		// Check if the exception really is the result first
-		if (e.name != self.name()) throw e;
+		if (e instanceof Error) throw e;
 		flat = e;
 	}
 

lib/supply.js
@@ -82,22 +82,25 @@ Supply.prototype.flatten = function () {
 		});
 	});
 
-	var values = { };
+	var properties = { };
+	var propertyValues = { };
 	_.each(self.properties(), function (property) {
-		if (!values[property.property.from._id()]) values[property.property.from._id()] = { };
-		values[property.property.from._id()][property.property.name()] = property.value;
+		if (!properties[property.property.from._id()]) {
+			properties[property.property.from._id()] = { };
+			propertyValues[property.property.from._id()] = { };
+		}
+		properties[property.property.from._id()][CryptoJS.MD5(property.property.name()).toString()] = property.flatten();
+		propertyValues[property.property.from._id()][property.property.name()] = property.value;
 	});
 
 	var flat = {
 		types: _.map(self.types(), function (type) {
 			return _.extend({
-				text: type.getText(values),
+				text: type.getText(propertyValues),
 			}, type.toReference());
 		}),
 		allTypes: _.values(allTypes),
-		properties: _.map(self.properties(), function (property) {
-			return property.flatten();
-		}),
+		properties: properties,
 		source: self.source(),
 		date: self.date(),
 		notes: self.notes(),

lib/type.js
@@ -227,7 +227,7 @@ Meteor.methods({
 				} catch (e) {
 					// The result being thrown as an exception signals a failed cascading update; just set needsReview
 					// Check if the exception really is the result first
-					if (e.name != supply.name) throw e;
+					if (e instanceof Error) throw e;
 					Supplies.update(supply._id, { $set: { needsReview: true } }, { multi: true });
 				}
 				if (flat && !isUnchanged(flat, supply)) {
@@ -243,7 +243,7 @@ Meteor.methods({
 				} catch (e) {
 					// The result being thrown as an exception signals a failed cascading update; just set needsReview
 					// Check if the exception really is the result first
-					if (e.name != protocol.name) throw e;
+					if (e instanceof Error) throw e;
 					Protocols.update(protocol._id, { $set: { needsReview: true } }, { multi: true });
 				}
 				if (flat && !isUnchanged(flat, protocol)) {