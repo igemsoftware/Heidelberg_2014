d94ba8945219e743360a2b12ad88cd3766c7eaa6
Silvan Schmitz <silvan@silvanschmitz.de>
2014-07-08 11:22:14 +0200

    publish user names

--- server/publish.js
+++ server/publish.js
@@ -1,5 +1,5 @@
 Meteor.publish('Everything', function() {
 	if(this.userId){
-		return [Types.find(), Supplies.find(), Protocols.find(), Experiments.find(), Options.find()];
+		return [Types.find(), Supplies.find(), Protocols.find(), Experiments.find(), Options.find(), Meteor.users.find({ }, { username: true })];
 	}
-})
+});
