ea077414db078a691feb8c52415413148812bfec
Constantin Ahlmann <artjom31415@googlemail.com>
2014-07-10 16:15:55 +0200

    Updated MainTester.java

--- MainTester.java
+++ MainTester.java
@@ -3,6 +3,8 @@ public class MainTester {
 	public static void main(String[] args){
 		System.out.println("Main Tester");
 
+        System.setErr(null);
+
 		System.out.println("Return of BoincAPIWrapper.init(): "+BoincAPIWrapper.init());
 
 		System.out.println("Finished Main Test");
