cc08acf270df5e6cae272458ecb8c4f077674a74
Silvan Schmitz <silvanschmitz@gmail.com>
2014-09-02 02:34:27 +0200

    redesigned experimentsList and suppliesList routes and added filtering options. resolves #21 and #43

--- client/experimentList.js
+++ client/experimentList.js
@@ -4,19 +4,67 @@ UI.registerHelper('experiments', function () {
 	return Experiments.find({ }, { sort: { finishDate: -1 } });
 });
 
-Template.experimentList.products = function () {
-	return _.values(this.products);
-}
+function ExperimentsListVM() {
+	var self = this;
+	self.manager = new OMManager();
 
-Template.experimentList.protocol = function () {
-	return Protocols.findOne(this.protocol._id);
-}
-
-Template.experimentList.productText = function (experiment) {
-	var texts = [];
-	var version = experiment.v[experiment.v.length - 1];
-	_.each(version.products[CryptoJS.MD5(this.name).toString()], function (productType) {
-		if (productType.text) texts.push(productType.text);
+	self.protocols = ko.observableArray();
+	self.allProtocolsComputedObservable = ko.computed(function () {
+		if (self.allProtocolsComputation) self.allProtocolsComputation.stop();
+		self.allProtocolsComputation = Deps.autorun(function () {
+			self.protocols(Protocols.find({ }, { sort: { name: 1 } }).map(function (protocol) {
+				return self.manager.getProtocol(protocol._id, undefined, protocol);
+			}));
+		});
 	});
-	return texts.join('/');
+	self.protocolFilter = ko.observable();
+
+	self.sortOptions = [{ text: 'A-Z', mongo: { 'protocol.name': 1 } }, { text: 'Z-A', mongo: { 'protocol.name': -1 } }, { text: 'Oldest', mongo: { finishDate: 1 } }, { text: 'Newest', mongo: { finishDate: -1 } }];
+	self.sort = ko.observable(self.sortOptions[3]);
+
+	self.experiments = ko.observableArray();
+	self.allExperimentsComputedObservable = ko.computed(function () {
+		var protocolFilter = self.protocolFilter();
+
+		if (self.allExperimentsComputation) self.allExperimentsComputation.stop();
+		self.allExperimentsComputation = Deps.autorun(function () {
+			self.experiments(Experiments.find(protocolFilter ? { 'protocol._id': protocolFilter._id() } : { }, { sort: self.sort().mongo }).map(function (experiment) {
+				return self.manager.getExperiment(experiment._id, undefined, experiment);
+			}));
+		});
+	});
+
 }
+ExperimentsListVM.prototype.constructor = ExperimentsListVM;
+
+ExperimentsListVM.prototype.cleanup = function () {
+	self.allExperimentsComputation.stop();
+	self.allExperimentsComputedObservable.dispose();
+	self.allProtocolsComputation.stop();
+	self.allProtocolsComputedObservable.dispose();
+};
+
+Template.experimentList.rendered = function () {
+	var self = this;
+	self.vm = new ExperimentsListVM();
+
+	self.nodesToClean = [];
+	setTimeout(function () {
+		for (var node = self.firstNode; node; node = node.nextSibling) {
+			// apply bindings to each direct child element of the template
+			// this does not apply to comments, i. e. containerless binding syntax!
+			// to enable both, we'd need to parse the comments ourselves
+			if (node.nodeType == Node.ELEMENT_NODE) {
+				ko.applyBindings(self.vm, node);
+				self.nodesToClean.push(node);
+			}
+		}
+	}, 0);
+};
+
+Template.experimentList.destroyed = function () {
+	_.each(this.nodesToClean, function (node) {
+		ko.cleanNode(node);
+	});
+	this.vm.cleanup();
+};
--- client/suppliesList.js
+++ client/suppliesList.js
@@ -4,20 +4,47 @@ function SuppliesListVM() {
 	var self = this;
 	self.manager = new OMManager();
 
+	self.types = ko.observableArray();
+	self.allTypesComputedObservable = ko.computed(function () {
+		if (self.allTypesComputation) self.allTypesComputation.stop();
+		self.allTypesComputation = Deps.autorun(function () {
+			self.types(Types.find({ }, { sort: { name: 1 } }).map(function (type) {
+				return self.manager.getType(type._id, undefined, type);
+			}));
+		});
+	});
+	self.typeFilter = ko.observable();
+
 	self.sortOptions = [{ text: 'A-Z', mongo: { 'types.0.text': 1 } }, { text: 'Z-A', mongo: { 'types.0.text': -1 } }, { text: 'Oldest', mongo: { date: 1 } }, { text: 'Newest', mongo: { date: -1 } }];
 	self.sort = ko.observable(self.sortOptions[0]);
 
-	var supplies = ko.computed(function () {
-		console.log(self.sort().mongo);
-		return ko.meteor.find(Supplies, { }, { sort: self.sort().mongo })();
-	});
-	self.supplies = ko.computed(function () {
-		return _.map(supplies(), function (supply) {
-			return self.manager.getSupply(supply._id(), undefined, ko.toJS(supply));
+	self.supplies = ko.observableArray();
+	self.allSuppliesComputedObservable = ko.computed(function () {
+		var typeFilter = self.typeFilter();
+
+		if (self.allSuppliesComputation) self.allSuppliesComputation.stop();
+		self.allSuppliesComputation = Deps.autorun(function () {
+			self.supplies(Supplies.find(typeFilter ? { 'allTypes._id': typeFilter._id() } : { }, { sort: self.sort().mongo }).map(function (supply) {
+				return self.manager.getSupply(supply._id, undefined, supply);
+			}));
 		});
 	});
+
 }
 SuppliesListVM.prototype.constructor = SuppliesListVM;
+
+SuppliesListVM.prototype.cleanup = function () {
+	self.allSuppliesComputation.stop();
+	self.allSuppliesComputedObservable.dispose();
+	self.allTypesComputation.stop();
+	self.allTypesComputedObservable.dispose();
+};
+
+SuppliesListVM.prototype.constructor = SuppliesListVM;
+
+SuppliesListVM.prototype.setTypeFilter = function (type) {
+	this.typeFilter(type);
+};
 
 Template.suppliesList.rendered = function () {
 	var self = this;
@@ -41,4 +68,5 @@ Template.suppliesList.destroyed = function () {
 	_.each(this.nodesToClean, function (node) {
 		ko.cleanNode(node);
 	});
+	this.vm.cleanup();
 };
